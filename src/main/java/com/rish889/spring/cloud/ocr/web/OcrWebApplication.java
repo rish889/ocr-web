c82131ae57366116434e130e64486f92096dfdc7
Garcha <rishabhg@amazon.com>
2016-12-18 21:07:57 -0800

    First Commit

---
+++ src/main/java/com/rish889/spring/cloud/ocr/web/OcrWebApplication.java
@@ -0,0 +1,12 @@
+package com.rish889.spring.cloud.ocr.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OcrWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OcrWebApplication.class, args);
+	}
+}
---
+++ src/test/java/com/rish889/spring/cloud/ocr/web/OcrWebApplicationTests.java
@@ -0,0 +1,16 @@
+package com.rish889.spring.cloud.ocr.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OcrWebApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
